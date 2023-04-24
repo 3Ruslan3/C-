618ffc6ce8c8fbac88c1063f7ca0f5eeb38c729e
Ruslan Seif <revolvman@gmail.com>
2023-04-24 21:19:49 +0300

    homework 5 added

--- Program.cs
+++ Program.cs
@@ -928,19 +928,329 @@
 // // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // // 6, 1, 33 -> [6, 1, 33]
 
-int[] array = new int[8];
+// int[] array = new int[8];
 
-int[] fillArrayWithNumbers(){
-    Console.WriteLine("Введите массив из 8 целых чисел, используя как разделитель ',' ");
-        string[] str = Console.ReadLine().Split(new char[] {','});
+// int[] fillArrayWithNumbers(){
+//     Console.WriteLine("Введите массив из 8 целых чисел, используя как разделитель ',' ");
+//         string[] str = Console.ReadLine().Split(new char[] {','});
 
-        Console.WriteLine(String.Join(", ", str) + " это массив символов");
+//         Console.WriteLine(String.Join(", ", str) + " это массив символов");
 
-        for (int i = 0; i < str.Length; i++){
-            array[i] = Convert.ToInt32(str[i]);
-        }
-    return array;
+//         for (int i = 0; i < str.Length; i++){
+//             array[i] = Convert.ToInt32(str[i]);
+//         }
+//     return array;
+// }
+
+// fillArrayWithNumbers();
+// Console.WriteLine(String.Join(", ", array) + " а это уже массив чисел");
+
+
+
+
+
+
+
+
+
+
+
+// Задача 31: Задайте массив из 12 элементов, заполненный
+//  случайными числами из промежутка [-9, 9]. 
+//  Найдите сумму отрицательных и положительных
+//   элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6]
+//  сумма положительных чисел равна 29, сумма отрицательных
+//   равна -20.
+
+// int[] numArr = new int[12];
+
+// int positiveNum=0;
+// int negNum=0;
+
+// for(int i=0;i<numArr.Length;i++)
+// {
+//     numArr[i] = new Random().Next(-9,10);
+
+//     if(numArr[i]>0)
+//     {
+//         positiveNum+=numArr[i];
+//     }
+//     else
+//     {
+//         negNum+=numArr[i];
+//     }
+// }
+
+// Console.WriteLine(String.Join(",",numArr)+
+// "\nСумма положительных "+positiveNum+" отрицательных "+negNum);
+
+
+
+
+
+
+
+
+
+// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// int[] numArr = new int[] {1,7898,66,-6,-52,0};
+
+// Console.WriteLine(String.Join(",",numArr));
+
+// for(int i=0;i<numArr.Length;i++)
+// {
+//     numArr[i]=-numArr[i];
+// }
+
+// Console.WriteLine(String.Join(",",numArr));
+
+
+
+
+
+
+
+
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// Console.WriteLine("Ведите число");
+// int nom = Convert.ToInt32(Console.ReadLine());
+
+// int[] numArr = new int[] { 1, 7898, 66, -6, -52, 0 };
+
+// Console.WriteLine(String.Join(",", numArr));
+
+// bool flag = false;
+
+// for (int i = 0; i < numArr.Length; i++)
+// {
+//     if (numArr[i] == nom)
+//     {
+//         flag = true;
+//         break;
+//     }
+// }
+// if (flag == true)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+
+
+
+
+
+
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+
+
+
+// Задача 35: Задайте одномерный массив из 123 случайных
+//  чисел. Найдите количество элементов массива, 
+//  значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. 
+// В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+// int[] numArr = new int[123];
+
+// int num = 0;
+
+// for (int i = 0; i < numArr.Length; i++)
+// {
+//     numArr[i] = new Random().Next(0, 501);
+
+//     if ((numArr[i] > 9) && (numArr[i] < 100))
+//     {
+//         num = num + 1;
+//     }
+// }
+
+// Console.WriteLine(String.Join(",", numArr));
+// Console.WriteLine("Количество двухзначных чисел = " + num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+// int[] numArr = new int[]{2, 5, 7 ,8};
+
+
+// int[] array1 = new int[] {3,8,2,6,2};
+// int[] array2= new int [(array1.Length/2)+ (array1.Length%2)];
+
+// for(int i=0;i<array2.Length;i++)
+// {
+//     array2[i]=array1[i]*array1[array1.Length-1-i];
+
+//     if (i==array1.Length-1-i)
+//     {
+//         array2[i]=array1[array1.Length-1-i];
+//     }
+// }
+
+// Console.WriteLine(String.Join(",",array2));
+
+
+
+
+
+
+
+
+// int[] arr = new int[5] {3,8,2,6,5}; //первый массив
+// int lg = arr.Length;                //задаем переменную для длинны первого массива для простоты формул
+// int middle = lg/2 + lg%2;           //узнаем длину второго массива(четный или нет)
+// int[] arr2 = new int[middle];       //задаем длину второго массива
+
+// for(int i=0;i<middle;i++)
+// {
+//     arr2[i]=arr[i]*arr[lg-i-1];  //берем пары элементов(1 и последний, 2й и предпоследний...) и перемножаем
+
+//     if(i==lg-1-i)               //если массив не четный
+//     {
+//         arr2[i]=arr[lg-1-i];    //то записать центральный элемент в конец второго массива
+//     }
+// }
+// Console.WriteLine(String.Join(",",arr2)); // выводим второй массив через запятую
+
+
+
+
+
+
+
+
+
+
+// // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// // [345, 897, 568, 234] -> 2
+// int temp = 0;
+
+// int[] array = new int[10];
+// for (int i = 0; i < array.Length; i++){
+//     array[i] = new Random().Next(100, 1000);
+// }
+// Console.WriteLine(String.Join(",", array));
+// for (int i = 0; i < array.Length; i++){
+//     if(array[i] % 2 == 0){
+//         temp++;
+//     }
+// }
+// Console.WriteLine("Число чётный чисел = " + temp);
+
+
+
+
+
+
+
+
+// // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// // [3, 7, 23, 12] -> 19
+// // [-4, -6, 89, 6] -> 0
+
+// int[] array = new int[] {-4, -6, 89, 6};
+// // for (int i = 0; i < array.Length; i++){
+// //     array[i] = new Random().Next(1, 100);
+// // }
+// Console.WriteLine(String.Join(",", array));
+
+// int sum = 0;
+
+// for (int i = 0; i < array.Length; i++){
+//     if (i % 2 > 0){
+//         sum = sum + array[i];
+//     }
+// }
+// Console.WriteLine("Сумма нечётных чисел = " + sum);
+
+
+
+
+
+
+
+
+
+
+
+// // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// // [3 7 22 2 78] -> 76
+
+// double[] array = new double[] {3, 7, 22, 2, 78};
+// double min = 0;
+// double max = 0;
+// double result = 0;
+
+// min = array[0];
+// for(int i = 0; i < array.Length; i++){
+//     if(array[i] < min){
+//         min = array[i];
+//     }
+//     if(array[i] > max){
+//         max = array[i];
+//     }
+// }
+// result = max - min;
+// Console.WriteLine("min = " + min + " max = " + max + "\n" + "Разница между максимальным и минимальным числами = " + result);
+
+
+
+
+
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] array = new int[] { 1, 2, 3, 4, 5 };
+// int[] array = new int[] { 6, 7, 3, 6 };
+
+int[] result = new int[(array.Length / 2) + (array.Length % 2)];
+
+for (int i = 0; i < result.Length; i++)
+{
+    result[i] = array[i] * array[array.Length - 1 - i];
+    if(i == array.Length - 1 - i){
+        result[i] = array[array.Length - 1 - i];
+    }
 }
 
-fillArrayWithNumbers();
-Console.WriteLine(String.Join(", ", array) + " а это уже массив чисел");
+Console.WriteLine(String.Join(",", array));
+Console.WriteLine(String.Join(",", result));
